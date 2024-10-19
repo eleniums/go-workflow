0944aa710a85b5af882e107834587bb6419b6d6b
eleniums <eleniums@yahoo.com>
2024-10-18 17:48:40 -0700

    Add Catch function to handle errors

--- action.go
+++ action.go
@@ -80,6 +80,22 @@ func If[T any](condition func(in T) (bool, error), ifTrue Action, ifFalse Action
 	}
 }
 
+// Executes an action and calls the handle function if an error occurs.
+func Catch(action Action, handle Action) Action {
+	return func(in any) (any, error) {
+		out, err := action(in)
+		if err != nil {
+			return handle(in)
+		}
+		return out, nil
+	}
+}
+
+func Retry() Action {
+	// TODO: implement retry function
+	return nil
+}
+
 // Returns an action that does nothing and returns nil.
 func NoOp() Action {
 	return func(in any) (any, error) {
