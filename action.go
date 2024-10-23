b077c7c3c9d18e34dac8a7a71506a8f336cee57d
eleniums <eleniums@yahoo.com>
2024-10-22 18:05:48 -0700

    Add comment

--- action.go
+++ action.go
@@ -103,6 +103,7 @@ func Finally(action Action, finally func(any, error) (any, error)) Action {
 func Retry(action Action, shouldRetry func(out any, err error) bool, maxRetries int, maxDelay time.Duration) Action {
 	return func(in any) (any, error) {
 		// calculate starting delay based on the max possible delay
+		// max delay divided by max retries to the power of 2
 		delay := maxDelay / (1 << maxRetries)
 
 		// first loop is the initial try and does not count as a retry
